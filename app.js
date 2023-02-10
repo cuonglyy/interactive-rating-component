cf603e04eb4f948ff3cf541d390cae21ac5c0be8
Cuong Ly <cuonglyy@gmail.com>
2023-02-10 15:21:53 -0800

    Able to add styling to active state on buttons that are clicked

--- app.js
+++ app.js
@@ -3,8 +3,20 @@ const formBtns = document.querySelectorAll('.btn');
 const submitBtn = document.querySelector('.submit-btn');
 
 
-const selectValue = (e) => {
-    console.log(e);
+
+const selectRating = (e) => {
+    /*
+        For each button, add an active state for the ones that are selected
+    */
+   formBtns.forEach(btn => {
+    if (btn.classList.contains('active')) {
+        btn.classList.remove('active');
+    }
+   });
+
+    e.target.classList.add('active');
+    localStorage.setItem('ratingValue', e.target.value);
+    console.log(localStorage.getItem('ratingValue'));
 }
 
 const submitForm = (e) => {
@@ -14,5 +26,5 @@ const submitForm = (e) => {
 
 form.addEventListener('click', submitForm);
 formBtns.forEach(btn => {
-    btn.addEventListener('click', selectValue);
+    btn.addEventListener('click', selectRating);
 })
