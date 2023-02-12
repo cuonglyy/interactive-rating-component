782c08505c2c7774c7948bfea04917d424f35d1d
Cuong Ly <cuonglyy@gmail.com>
2023-02-11 18:26:41 -0800

    Added thank you state

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const form = document.querySelector('.rating-form');
 const formBtns = document.querySelectorAll('.btn');
-const submitBtn = document.querySelector('.submit-btn');
+const submitBtn = document.querySelector('.form__submit-btn');
+const ratingState = document.querySelector('#rating-state');
 
 
 
 const selectRating = (e) => {
     /*
-        For each button, add an active state for the ones that are selected
+        For each button, add an active state for the ones that are selected.
+        If any buttons are active, then remove the active state and add it
+        to the buttons that's clicked
     */
    formBtns.forEach(btn => {
     if (btn.classList.contains('active')) {
@@ -19,12 +22,17 @@ const selectRating = (e) => {
     console.log(localStorage.getItem('ratingValue'));
 }
 
+const switchState = (e) => {
+    ratingState.style.display = 'none';
+}
+
 const submitForm = (e) => {
     e.preventDefault();
 }
 
 
 form.addEventListener('click', submitForm);
+submitBtn.addEventListener('click', switchState);
 formBtns.forEach(btn => {
     btn.addEventListener('click', selectRating);
 })
